3ee4cb8f8d4bb9f80b00b8919ecd95cc6aeda14c
rcherukuri12 <raja_cherukuri@hotmail.com>
2021-03-16 12:13:39 -0700

    add code

--- libsvm_reader/src/libsvm_reader.jl
+++ libsvm_reader/src/libsvm_reader.jl
@@ -3,4 +3,22 @@ module libsvm_reader
 export read
 using SparseArrays
 
+
+function get_sparse_vector_array(fname::String)
+    labels = Float32[]
+    data = []
+    max_feature_index = 0
+    fi = open(fname,"r")
+    for line in eachline(fi)
+        # remove comments
+        line = rstrip(split(line,"#")[1])
+        #split the line into array
+        line = split(line," ")
+        #store the label
+        push!(labels,parse(Float32,line[1]))
+
+    end
+    close(fi)
+end
+
 end
